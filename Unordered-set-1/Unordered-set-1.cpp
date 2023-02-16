b441b466d6828a9f598d9b1df7a3f40d531a0db8
Tetyana-K <tanya.kyryk@gmail.com>
2023-02-16 18:46:32 +0200

    Add project files.

---
+++ Unordered-set-1/Unordered-set-1.cpp
@@ -0,0 +1,12 @@
+// Unordered-set-1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <unordered_set>
+using namespace std;
+int main()
+{
+    unordered_set  <int> s{ 10, 2, 34, 5 };
+}
+
+
