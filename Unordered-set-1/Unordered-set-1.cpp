449fc32750e4418cd6f1dc2ce0e468bea228240e
Tetyana-K <tanya.kyryk@gmail.com>
2023-02-16 18:54:41 +0200

    add fun printCont()

--- Unordered-set-1/Unordered-set-1.cpp
+++ Unordered-set-1/Unordered-set-1.cpp
@@ -4,9 +4,19 @@
 #include <iostream>
 #include <unordered_set>
 using namespace std;
+template<typename T>
+void printCont(const T& cont)
+{
+	for (auto& i : cont)
+	{
+		cout << "\t" << i;
+	}
+	cout << endl;
+}
 int main()
 {
     unordered_set  <int> s{ 10, 2, 34, 5 };
+	printCont(s);
 }
 
 
